22c5bbd4ef09da124539cf8a5f06ee572f09a55d
mrdavehun21 <ernhofferd@gmail.com>
2025-01-11 22:21:36 +0100

    backend update

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@ const app = express();
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
-const PORT = 5000;
 
+// Use the PORT environment variable or default to 5000
+const PORT = process.env.PORT || 5000;
+
+app.use(cors()); // Allow requests from other origins
+app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
