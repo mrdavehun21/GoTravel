ec5fe3c92feba7e63dfa83f0cea421b299fc0d73
mrdavehun21 <ernhofferd@gmail.com>
2025-01-11 21:17:20 +0100

    backend test

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config();
 const PORT = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Backend is running!');
+app.use(cors()); // Allow requests from other origins
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
